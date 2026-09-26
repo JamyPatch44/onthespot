bfbafcf6d002ba91fc5b8ae43988b26bc799fa2c
0x4B1D <samuel.brizzi94@gmail.com>
2026-09-26 13:33:27 +0200

    Add 'song' type to Apple Music URL regex
    Signed-off-by: 0x4B1D <samuel.brizzi94@gmail.com>

--- api/src/onthespot/resources/regexes.py
+++ api/src/onthespot/resources/regexes.py
@@ -5,7 +5,7 @@ import re
 
 # Audio services
 APPLE_MUSIC_URL_REGEX = re.compile(
-    r"https?://music.apple.com/([a-z]{2})/(?P<type>album|playlist|artist)"
+    r"https?://music.apple.com/([a-z]{2})/(?P<type>album|playlist|artist|song)"
     r"(?:/(?P<title>[-a-z0-9]+))?/(?P<id>[\w.-]+)"
     r"(?:\?i=(?P<track_id>\d+))?(?:&.*)?$"
 )
